156c9417aec6e02344af30e5b978a326f2d30157
liuxiaobleach <1241368737@qq.com>
2023-03-29 19:20:56 +0800

    update

--- tests/rlp/test.c
+++ tests/rlp/test.c
@@ -5,11 +5,11 @@
 #include "rlp.h"
 
 struct rlpItemAllocator itemAllocator;
+uint8_t buf[2048];
 
 __attribute__((visibility("default")))
 int zkmain() {
     uint32_t length = (uint32_t)wasm_input(1);
-    uint8_t buf[1024];
     read_bytes_from_u64(buf, length, 0);
     struct rlpItem *root = decode(buf, 0, &itemAllocator);
     struct rlpItem *anchor = root->firstChild;
